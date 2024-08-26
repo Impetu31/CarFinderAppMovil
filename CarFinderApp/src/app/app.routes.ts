e3508bb80906ceaa3671f821707b3bcddb9e2e58
Your Name <car.montupil@duocuc.cl>
2024-08-26 15:48:23 -0400

    ...

--- CarFinderApp/src/app/app.routes.ts
+++ CarFinderApp/src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
+import { MainPage } from './main/main.page'; // Ajusta la ruta si es necesario
 
 export const routes: Routes = [
-  
+  { 
+    path: 'main', 
+    loadComponent: () => import('./main/main.page').then(m => m.MainPage) 
+  }
 ];
